b39e6a75790f6fb47ef114860a55a0e9ceefd948
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-04-15 22:01:10 +0200

    43-todo-list

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const link = document.querySelector("a");
-let brojac = 0;
+const grandParent = document.querySelector(".grand-parent");
+const children = grandParent.querySelectorAll(".child");
+const parentOne = children[0].parentElement;
 
-link.addEventListener("click", e => {
-    e.preventDefault();
-    console.log(`kliknuo ${++brojac}`);
-});
+children.forEach(child => child.style.color = "red");
+parentOne.style.color = "green";
