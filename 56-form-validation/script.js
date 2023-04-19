e67e718e4acdc54e8688314d827b622755ad655c
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-04-19 11:55:35 +0200

    56-form-validation

--- 56-form-validation/script.js
+++ 56-form-validation/script.js
@@ -1,5 +1,12 @@
 // TODO: Select all elements needed
 //       Use the HTML to figure out what classes/ids will work best for selecting each element
+const form = document.querySelector("#form");
+const errorsContainer = document.querySelector(".errors");
+const errorList = document.querySelector(".errors-list");
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+const passwordConfirmationInput = document.querySelector("#password-confirmation");
+const termsInput = document.querySelector("#terms");
 
 // TODO: Create an event listener for when the form is submitted and do the following inside of it.
 //       TODO: Create an array to store all error messages and clear any old error messages
@@ -9,6 +16,29 @@
 //             3. Ensure the password and confirmation password match
 //             4. Ensure the terms checkbox is checked
 //       TODO: If there are any errors then prevent the form from submitting and show the error messages
+form.addEventListener("submit", e => {
+    clearErrors();
+    let errors = false;
+    if (usernameInput.value.length < 6) {
+        showErrors("Username must be at least 6 character long.");
+        errors = true;
+    }
+    if (passwordInput.value.length < 10) {
+        showErrors("Password must be at least 10 character long.");
+        errors = true;
+    }
+    if (passwordConfirmationInput.value !== passwordInput.value) {
+        showErrors("Password must match.");
+        errors = true;
+    }
+    if (termsInput.checked === false) {
+        showErrors("You must accept the tearms.");
+        errors = true;
+    }
+    if (errors) {
+        e.preventDefault();
+    }
+});
 
 // TODO: Define this function
 function clearErrors() {
@@ -20,6 +50,11 @@ function clearErrors() {
     // set the innerHTML property of the error-list to an empty string and that will also clear the children.
     // I recommend trying to accomplish this with a while loop, though, for practice.
     // Also, make sure you remove the show class to the errors container
+    while (errorList.hasChildNodes()) {
+        errorList.removeChild(errorList.firstChild);
+    }
+    // errorList.innerHTML = "";
+    errorsContainer.classList.remove("show");
 }
 
 // TODO: Define this function
@@ -27,4 +62,8 @@ function showErrors(errorMessages) {
     // Add each error to the error-list element
     // Make sure to use an li as the element for each error
     // Also, make sure you add the show class to the errors container
+    const errorListItem = document.createElement("li");
+    errorListItem.innerText = errorMessages;
+    errorList.appendChild(errorListItem);
+    errorsContainer.classList.add("show");
 }
